0ac217ce7c166e8719d8fa2b4ce856581772a302
Neonpk (Chichard) <15403440+Neonpk@users.noreply.github.com>
2024-04-02 18:53:51 +0500

    Fixed warning in the converter.

--- ExchangeRates/Converters/InverseBooleanConverter.cs
+++ ExchangeRates/Converters/InverseBooleanConverter.cs
@@ -13,6 +13,6 @@ public class InverseBooleanConverter : IValueConverter
 
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        throw new NotSupportedException();
     }
 }
